45a834fb11903c052f87a7f7cf2db3314a5227a9
Coen Mathijssen <mathijssen.coen@gmail.com>
2020-05-25 17:18:08 +0200

    #09 - Coen: now able to login on Spotify and fetch an access and refresh token. This token enables the user to use his spotify account on the website

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
 const config = require('./config');
 
 const express = require('express'),
-    router = require('./scripts/modules/router'),
-    app = express();
+  router = require('./scripts/modules/router'),
+  app = express();
 
 app.set('view engine', 'ejs')
-    .set('views', 'views')
-    .use(express.static('static'))
+  .set('views', 'views')
+  .use(express.static('static'))
 
-    .get('/', async (req, res) => {
-        router.basicPage(res, "home", "Home");
-    })
+  .get('/', async (req, res) => {
+    router.basicPage(res, "home", "Home");
+  })
 
-    .listen(config.port, () => {
-        console.log(`Application started on port: ${config.port}`);
-    });
+  .get('/login', async (req, res) => {
+    router.basicPage(res, "login", "Login");
+  })
+
+  .listen(config.port, () => {
+    console.log(`Application started on port: ${config.port}`);
+  });
+
+// Spotify Oauth
+const spotifyLogin = require('./server/login.js')
+const spotifyCallback = require('./server/callback.js')
+
+// Spotify login routes
+app.get('/spotifylogin', spotifyLogin) // Redirect for Spotify auth
+app.get('/callback', spotifyCallback) // Callback for fetching Spotify tokens
+
---
+++ server/callback.js
@@ -0,0 +1,45 @@
+const fetch = require('node-fetch')
+const queryString = require('query-string')
+
+const { SPOTIFY_REDIRECT_URI, SPOTIFY_CLIENT_SECRET, SPOTIFY_CLIENT_ID } = process.env
+
+module.exports = async (req, res, next) => {
+  console.log('running')
+
+  const code = req.query.code
+  const queryObject = {
+    grant_type: 'authorization_code',
+    code: code,
+    redirect_uri: SPOTIFY_REDIRECT_URI
+  }
+
+  const fetchOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': `Basic ${encodeToBase64(`${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`)}`
+    }
+  }
+
+  const query = queryString.stringify(queryObject)
+  const url = `https://accounts.spotify.com/api/token?${query}`
+
+  try {
+    const spotifyResponse = await fetch(url, fetchOptions)
+    const data = await spotifyResponse.json()
+
+    console.log('succesful: ', data)
+
+    res.cookie('ACCESS_TOKEN', data.access_token)
+    res.cookie('REFRESH_TOKEN', data.refresh_token)
+
+    res.redirect('/home')
+  } catch (err) {
+    console.log('error verifying: ', err)
+    res.send(err)
+  }
+}
+
+function encodeToBase64 (text) {
+  return new Buffer.from(text).toString('base64')
+}

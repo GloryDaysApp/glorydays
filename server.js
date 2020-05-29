b42f20b20fd63be3eaa8c5e05d1688af0a06c6f6
marissaverdonck <marissa.verdonck@hva.nl>
2020-05-29 12:50:07 +0200

    #19-  marissa: -conversation UI colors, animation

--- server.js
+++ server.js
@@ -2,20 +2,24 @@ const config = require('./config');
 
 const express = require('express'),
   router = require('./scripts/modules/router'),
-  app = express()
-
-let caregivers = { id: "test" }
+  app = express(),
+  multer = require('multer'),
+  upload = multer({ dest: 'uploads/' }),
+  caregivers = { id: "test" }
 
 app.set('view engine', 'ejs')
   .set('views', 'views')
   .use(express.static('static'))
 
 .get('/', async(req, res) => {
-    router.basicPage(res, "home", "Home");
-  })
-  .get('/addMemory.ejs', async(req, res) => {
-    router.pageWithData(res, "addMemory", "Herinnering toevoegen", caregivers);
-  })
+  router.basicPage(res, "home", "Home");
+})
+
+
+.get('/addMemory.ejs', async(req, res) => {
+  router.pageWithData(res, "addMemory", "Herinnering toevoegen", caregivers);
+
+})
 
 
 

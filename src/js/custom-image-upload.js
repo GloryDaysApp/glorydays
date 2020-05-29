a7564e1f2cd554e77a95ae2271aa96ad719a3c92
MarjoleinAardewijn <marjolein.aardewijn@gmail.com>
2020-05-29 16:22:57 +0200

    #33 - Marjolein: show and hide output image

--- src/js/custom-image-upload.js
+++ src/js/custom-image-upload.js
@@ -15,6 +15,9 @@ realFileButton.addEventListener('change', (event) => {
         customText.textContent = realFileButton.value.replace(/^.*[\\\/]/, '');
 
         // show chosen image
+        outputImage.classList.remove('hide');
+        outputImage.classList.add('show');
+
         outputImage.src = URL.createObjectURL(event.target.files[0]);
         outputImage.onload = () => {
             URL.revokeObjectURL(outputImage.src); // free memory
@@ -23,5 +26,7 @@ realFileButton.addEventListener('change', (event) => {
         // if no file is chosen, reset everything to default values
         customText.textContent = 'Nog geen afbeelding gekozen.';
         outputImage.src = '';
+        outputImage.classList.remove('show');
+        outputImage.classList.add('hide');
     }
 });

8b857173956155c6916415d26d8ffeea5dc47214
Coen Mathijssen <mathijssen.coen@gmail.com>
2020-06-08 13:33:58 +0200

    #51 - Coen: fixed indenting in router.js

--- scripts/modules/router.js
+++ scripts/modules/router.js
@@ -1,13 +1,13 @@
 const basicPage = (res, view, title) => {
-  res.render(view, {
-    title,
-  });
+    res.render(view, {
+        title,
+    });
 };
 pageWithData = (res, view, title, data) => {
-  res.render(view, {
-    title,
-    data
-  });
+    res.render(view, {
+        title,
+        data
+    });
 };
 
 module.exports = { basicPage, pageWithData };

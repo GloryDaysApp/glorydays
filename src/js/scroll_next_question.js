f9501adf952d9fa5cc76c50d4a1e6256b598cf88
Simone van Zeijl <simonev.zeijl@hotmail.nl>
2020-06-11 14:59:36 +0200

    #71 - Simone: added scroll event to next question memoery

--- src/js/scroll_next_question.js
+++ src/js/scroll_next_question.js
@@ -1,30 +1,59 @@
+// Input checkbox
 const inputCheckbox = document.querySelector('input[type="checkbox"]');
 const checkboxes = document.getElementById('checkboxes');
+
+// Input text
+const inputText = document.querySelectorAll('input[type="text"]');
+const textFields = document.querySelectorAll('.input-field');
+
+// Current fieldset
 const currentFieldset = document.getElementsByClassName('active-fieldset');
 const currentDot = document.getElementsByClassName('active-navigation-dot');
 
-checkboxes.addEventListener('change', (e) => {
+checkboxes.addEventListener('change', checkboxChecked);
+
+// Function to check if checkbox is checked, and go to the next question
+function checkboxChecked() {
     const checkbox = event.target;
     const checked = checkbox.checked;
 
     if (checked) {
-        console.log('checked!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
         setTimeout(() => {
-            console.log('1 sec later');
-            console.log(currentFieldset);
-
-            console.log(currentFieldset[0].nextElementSibling);
-
-            currentFieldset[0].nextElementSibling.classList.remove('hidden-fieldset');
-            currentFieldset[0].nextElementSibling.classList.add('active-fieldset');
-
-            currentFieldset[0].classList.add('hidden-fieldset');
-            currentFieldset[0].classList.remove('active-fieldset');
-
-            currentDot[0].nextElementSibling.classList.add('active-navigation-dot');
-
-            currentDot[0].classList.remove('active-navigation-dot');
-
+            changeActiveState();
         }, 1000);
     }
-});
+}
+
+for (let i = 0; i < textFields.length; i++) {
+    textFields[i].addEventListener('change', nextQuestion);
+}
+
+// Function to check if input text has a value, and go to the next question
+function nextQuestion(e) {
+    const target = e.target || e.srcElement;
+    const targetValue = target.value;
+
+    if (targetValue != '') {
+        setTimeout(() => {
+
+            changeActiveState();
+
+            console.log('heeeee marjolein hoe gaat die met jou?');
+        }, 1000);
+    }
+}
+
+function changeActiveState() {
+    // Next fieldset
+    currentFieldset[0].nextElementSibling.classList.remove('hidden-fieldset');
+    currentFieldset[0].nextElementSibling.classList.add('active-fieldset');
+
+    // Current fieldset
+    currentFieldset[0].classList.add('hidden-fieldset');
+    currentFieldset[0].classList.remove('active-fieldset');
+
+    // Navigation dot sidebar
+    currentDot[0].nextElementSibling.classList.add('active-navigation-dot');
+    currentDot[0].classList.remove('active-navigation-dot');
+}
+

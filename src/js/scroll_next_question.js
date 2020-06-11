c4d0bbaf339878712f3b48fb0410b224dcbc3c58
Simone van Zeijl <simonev.zeijl@hotmail.nl>
2020-06-11 12:03:02 +0200

    #71 - Simone: added scroll page

---
+++ src/js/scroll_next_question.js
@@ -0,0 +1,10 @@
+const input = document.querySelectorAll('input');
+console.log('input: ', input);
+
+// function checkInput() {
+// if (input.value.lenght != null) {
+// if (input !== null && input.value !== '') {
+if (input.length >= 1) {
+    console.log('Input Checked');
+}
+// }

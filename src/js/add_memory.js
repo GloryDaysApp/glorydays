a7dc6624ba1c9412b62392fad684000eacead222
marissaverdonck <marissa.verdonck@hva.nl>
2020-06-16 09:40:58 +0200

    #90 - marissa: scroll buttons, fix animation bug, inputfields for textareas

--- src/js/add_memory.js
+++ src/js/add_memory.js
@@ -1,45 +1,45 @@
-// function AddSendButtonToInputTextFields() {
-//     const inputTextFields = Array.from(document.querySelectorAll('.input-text'));
-//     if (inputTextFields) {
-//         inputTextFields.forEach((element, index) => {
-//             inputTextFields[index].insertAdjacentHTML('beforeend', `
-//         <a class='send-button' id='send-button-${index}'>Stuur</a> 
-//         `);
-//         });
-//     }
-//     checkClickedInputSendButton();
-// }
+function AddSendButtonToInputTextFields() {
+    const inputTextFields = Array.from(document.querySelectorAll('.input-text'));
+    if (inputTextFields) {
+        inputTextFields.forEach((element, index) => {
+            inputTextFields[index].insertAdjacentHTML('beforeend', `
+            <img src="/icons/send.svg" alt="send button" id='send-button-${index}' class="send-button">
+        `);
+        });
+    }
+    checkClickedInputSendButton();
+}
 
-// function checkClickedInputSendButton() {
-//     const inputSendButtonsArray = Array.from(document.querySelectorAll('.send-button'));
-//     if (inputSendButtonsArray) {
-//         inputSendButtonsArray.forEach((element, index) => {
-//             inputSendButtonsArray[index].addEventListener('click', addTextBaloon(index));
-//         });
-//     }
-// }
+function checkClickedInputSendButton() {
+    const inputSendButtonsArray = Array.from(document.querySelectorAll('.send-button'));
+    if (inputSendButtonsArray) {
+        inputSendButtonsArray.forEach((element, index) => {
+            inputSendButtonsArray[index].addEventListener('click', addTextBaloon(index));
+        });
+    }
+}
 
-// function addTextBaloon(index) {
-//     return function () {
-//         const initialInputFieldArray = Array.from(document.querySelectorAll('.input-text input[type="text"]'));
-//         const initialInputArray = Array.from(document.querySelectorAll('.input-text'));
-//         let valueInitialInputField = initialInputFieldArray[index].value;
-//         const initialInputField1 = initialInputFieldArray[index];
-//         const initialInputField = initialInputArray[index];
+function addTextBaloon(index) {
+    return function () {
+        const initialInputFieldArray = Array.from(document.querySelectorAll('.input-text input[type="text"]'));
+        const initialInputArray = Array.from(document.querySelectorAll('.input-text'));
+        let valueInitialInputField = initialInputFieldArray[index].value;
+        const initialInputField1 = initialInputFieldArray[index];
+        const initialInputField = initialInputArray[index];
         
-//         if (valueInitialInputField != '') {
+        if (valueInitialInputField != '') {
         
-//             if(initialInputField1.id== 'keywords'){
-//                 initialInputField.insertAdjacentHTML('beforebegin', `
-//                 <div class='output'><textarea class='output-textarea output-keyword' rows='1' style='width:${valueInitialInputField.length + 1}rem;'>${valueInitialInputField}</textarea></div>`);
-//             }
-//             else{
-//                 initialInputField.insertAdjacentHTML('beforebegin', `
-//                 <div class='output'><textarea class='output-textarea' rows='1'>${valueInitialInputField}</textarea></div>`);
-//             }
-//         }
-//         initialInputFieldArray[index].value = '';
-//     };
-// }
+            if(initialInputField1.id== 'keywords'){
+                initialInputField.insertAdjacentHTML('beforebegin', `
+                <div class='output'><input class='output-textarea output-keyword' rows='1' style='width:${valueInitialInputField.length + 1}rem;' value='${valueInitialInputField}'/></div>`);
+            }
+            else{
+                initialInputField.insertAdjacentHTML('beforebegin', `
+                <div class='output'><input class='output-textarea' rows='1' value='${valueInitialInputField}'/></div>`);
+            }
+        }
+        initialInputFieldArray[index].value = '';
+    };
+}
 
-// AddSendButtonToInputTextFields();
+AddSendButtonToInputTextFields();

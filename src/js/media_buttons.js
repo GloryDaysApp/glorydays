8db7cdf8a3d37de103283e95b0bdff20a542357d
marissaverdonck <marissa.verdonck@hva.nl>
2020-06-16 11:09:44 +0200

    #93 - marissa:styling afmaken

--- src/js/media_buttons.js
+++ src/js/media_buttons.js
@@ -1,17 +1,17 @@
 const addImageButton = document.getElementById('button--add-image'),
     addVoiceRecordingButton = document.getElementById('button--add-voice-recording'),
-    addTextButton = document.getElementById('button--add-text'),
+    // addTextButton = document.getElementById('button--add-text'),
     addImageOutput = document.getElementById('output--add-image'),
-    addVoiceRecordingOutput = document.getElementById('output--add-voice-recoding'),
-    addTextOutput = document.getElementById('output--add-text');
+    addVoiceRecordingOutput = document.getElementById('output--add-voice-recoding');
+    // addTextOutput = document.getElementById('output--add-text');
 
-if (addImageButton && addVoiceRecordingButton && addTextButton) {
+if (addImageButton && addVoiceRecordingButton) {
     addImageButton.addEventListener('click', (event) => {
         event.preventDefault();
         console.log('image button click');
 
         addVoiceRecordingOutput.classList.remove('active');
-        addTextOutput.classList.remove('active');
+        // addTextOutput.classList.remove('active');
         addImageOutput.classList.toggle('active');
     });
 
@@ -20,16 +20,16 @@ if (addImageButton && addVoiceRecordingButton && addTextButton) {
         console.log('voice button click');
 
         addImageOutput.classList.remove('active');
-        addTextOutput.classList.remove('active');
+        // addTextOutput.classList.remove('active');
         addVoiceRecordingOutput.classList.toggle('active');
     });
 
-    addTextButton.addEventListener('click', (event) => {
-        event.preventDefault();
-        console.log('text button click');
+    // addTextButton.addEventListener('click', (event) => {
+    //     event.preventDefault();
+    //     console.log('text button click');
 
-        addImageOutput.classList.remove('active');
-        addVoiceRecordingOutput.classList.remove('active');
-        addTextOutput.classList.toggle('active');
-    });
+    //     addImageOutput.classList.remove('active');
+    //     addVoiceRecordingOutput.classList.remove('active');
+    //     addTextOutput.classList.toggle('active');
+    // });
 }

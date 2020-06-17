f1eca0f60aae9716d575da364046f9018cc6e8dd
MarjoleinAardewijn <marjolein.aardewijn@gmail.com>
2020-06-17 11:23:33 +0200

    #133 - Marjolein: code for offline page and caching html pages

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,12 +1,22 @@
-const CORE_CACHE_VERSION = 1,
+const CORE_CACHE_VERSION = 2,
     CORE_CACHE_NAME = `core-v${CORE_CACHE_VERSION}`,
+    // HTML_CACHE_NAME = `core-html-v${CORE_CACHE_VERSION}`,
     CORE_ASSETS = [
+        // '/offline',
         '/index.css',
         '/index.js'
     ];
 
 self.addEventListener('install', event => {
     console.log('Installing Service Worker');
+    // event.waitUntil(
+    //     Promise.all([
+    //         caches.open(CORE_CACHE_NAME).then(cache => cache.addAll(CORE_ASSETS)),
+    //         fetchAndCache('/', HTML_CACHE_NAME)
+    //     ])
+    //         .then( () => self.skipWaiting())
+    // );
+
     event.waitUntil(
         caches.open(CORE_CACHE_NAME)
             .then(cache => cache.addAll(CORE_ASSETS))
@@ -28,15 +38,58 @@ self.addEventListener('fetch', event => {
                 .then(cache => cache.match(event.request.url))
         );
     }
+    // } else if (isHtmlGetRequest(event.request)) {
+    //     console.log('HTML Get Request: ', event.request.url);
+    //     // generic fallback
+    //     event.respondWith(
+    //         caches.open(HTML_CACHE_NAME)
+    //             .then(cache => cache.match(event.request.url))
+    //             .then(response => response ? response : fetchAndCache(event.request, HTML_CACHE_NAME))
+    //             .catch(err => {
+    //                 return caches.open(CORE_CACHE_NAME)
+    //                     .then(cache => cache.match('/offline'));
+    //             })
+    //     );
+    // }
 });
 
 /**
- * Checks if a request is a core GET request
+ * Fetch url and add it to the cache
  *
- * @param {Object} request        The request object
- * @returns {Boolean}            Boolean value indicating whether the request is in the core mapping
+ * @param request
+ * @param cacheName
+ * @returns {Promise<Response | never>}
  */
-const isCoreGetRequest = (request) => {
+const fetchAndCache = (request, cacheName) => {
+        return fetch(request)
+            .then(response => {
+                if (!response.ok) {
+                    throw new TypeError('Bad response status');
+                }
+
+                const clone = response.clone();
+                caches.open(cacheName).then((cache) => cache.put(request, clone));
+                return response;
+            });
+    },
+
+    /**
+     * Checks if a request is a GET and HTML request
+     *
+     * @param {Object} request        The request object
+     * @returns {Boolean}            Boolean value indicating whether the request is a GET and HTML request
+     */
+    isHtmlGetRequest = (request) => {
+        return request.method === 'GET' && (request.headers.get('accept') !== null && request.headers.get('accept').indexOf('text/html') > -1);
+    },
+
+    /**
+     * Checks if a request is a core GET request
+     *
+     * @param {Object} request        The request object
+     * @returns {Boolean}            Boolean value indicating whether the request is in the core mapping
+     */
+    isCoreGetRequest = (request) => {
         return request.method === 'GET' && CORE_ASSETS.includes(getPathName(request.url));
     },
 
